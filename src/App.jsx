a4a94dd8232e92c82ad8ea8b4c50fe51772f06e3
najafsikander <najafsikander@yahoo.com>
2025-06-25 17:33:47 +0400

    Removed TODO as the page is completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero"
 import Characters from "./components/Characters"
 import Arena from "./components/Arena"
 import Footer from "./components/Footer"
-// TODO:Complete the home page
+
 const App = () => {
   return (
     <div>
